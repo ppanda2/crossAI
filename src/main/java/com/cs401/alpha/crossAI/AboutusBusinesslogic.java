52c3ddf50d0595a20f3e72504a1d15a775da5b2d
pratyushpanda <pratyush.panda1985@gmail.com>
2019-07-11 01:15:27 -0700

    june11

--- src/main/java/com/cs401/alpha/crossAI/AboutusBusinesslogic.java
+++ src/main/java/com/cs401/alpha/crossAI/AboutusBusinesslogic.java
@@ -8,7 +8,7 @@ public class AboutusBusinesslogic {
 	public List<Aboutus> getAboutusList()
 	{
 		List<Aboutus> listus = new ArrayList<>();
-		listus.add(new Aboutus("Pratysuh Panda", ""));
+		listus.add(new Aboutus("Pratysuh Panda", "fe8394"));
 		listus.add(new Aboutus("Raoni Marcelino", ""));
 		listus.add(new Aboutus("Jeremy Lee", ""));
 		listus.add(new Aboutus("Faisal Alnafea", ""));
